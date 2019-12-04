28d38574eef77a8cf424ba7d98480948fee1d154
jiaopi404 <1240659804@qq.com>
2019-12-04 08:38:26 -0800

    update: list demo

---
+++ my_test_003/demo_01_list.py
@@ -0,0 +1,12 @@
+# demo list
+my_list = ["hello", " ", "world"]
+
+print(my_list)
+
+my_list.append('a')
+print('after append, my_list is: %s' % my_list)
+
+print('my_list[2] is: %s' % my_list[2])
+
+del(my_list[2])
+print('after del my_list[2], my_list is: %s' % my_list)
